c6e597cb573372eb2eb16ed2ca8d73bf3f20892e
Nitin Chaturvedi <{chaturvediinitin@gmail.com}>
2025-07-18 22:59:55 +0530

    vercel

--- personal-library-tracker/client/vite.config.js
+++ personal-library-tracker/client/vite.config.js
@@ -1,3 +1,4 @@
+// client/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import path from 'path';
@@ -9,5 +10,7 @@ export default defineConfig({
       '@': path.resolve(__dirname, 'src'),
     },
   },
-  base: "/"
+  define: {
+    'process.env': {},
+  },
 });
